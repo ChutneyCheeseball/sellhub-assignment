293b2883f1266c655a066d9b124f5dbf074f8210
Dylan du Toit <dylan.du.toit@gmail.com>
2025-01-06 13:12:05 +0200

    Enforce inventory_count >= 0 as database constraint

--- apps/api/src/db/schema.ts
+++ apps/api/src/db/schema.ts
@@ -1,7 +1,14 @@
-import { pgTable, uuid, varchar, integer } from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
+import { pgTable, uuid, varchar, integer, check } from "drizzle-orm/pg-core";
 
-export const products = pgTable("products", {
-  id: uuid().primaryKey().defaultRandom(),
-  name: varchar().notNull(),
-  inventory_count: integer().notNull().default(0),
-});
+export const products = pgTable(
+  "products",
+  {
+    id: uuid().primaryKey().defaultRandom(),
+    name: varchar().notNull(),
+    inventory_count: integer().notNull().default(0),
+  },
+  // Enforce inventory_count being positive at database level
+  // Documentation is wrong, se:e https://github.com/drizzle-team/drizzle-orm/issues/3520
+  (table) => [check("inventory_check", sql`${table.inventory_count} >= 0`)]
+);
